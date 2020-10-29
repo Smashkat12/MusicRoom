ea91d30d7b9014d405168f857f81ca425ac95d0a
mmodisad <50734123+mmodisad@users.noreply.github.com>
2020-10-29 17:07:39 +0200

    dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,21 +1,21 @@
 module.exports = {
   MONGODB: {
     mongoURI:
-      "mongodb+srv://musicroom:musicroom@cluster0.icn4w.mongodb.net/musicroom?retryWrites=true&w=majority",
+      "mongodb+srv://Hypertube:Hypertube@cluster0.1mxy5.mongodb.net/Hypertube?retryWrites=true&w=majority",
   },
   FACEBOOK: {
-    clientID: "794995081281788",
-    secret: "eb6f4ac91e7833c57970c5c05097aaa7",
+    clientID: "1506029193118389",
+    secret: "d2ff913318fe9b7c02f76a45905c5776",
   },
   GOOGLE: {
     clientID:
       "257167475234-7va00kj9qh7hf4s6od5i7c5m6r7f44qv.apps.googleusercontent.com",
     secret: "VSsOEh4e1hDNO4oPR641H8BZ",
   },
-  DEEZER: {
+  FORTY_TWO: {
     clientID:
-      "437522",
-    secret: "370f784b6dc2d580d1b4e0a928999d4b",
+      "fce83e782081bef0dc781dd252038d3873eb7710438a1e1ef69ccbed3ef27285",
+    secret: "8bcfab08409ad1d42eb6ba6d11b07a643a999669a76d529d513a481be6c6ddd3",
   },
   JWT: {
     secret: "thesecretesourcetojwt",
