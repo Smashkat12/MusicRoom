43d5091b215df6e90424fb7864806d934bb33f20
Smashkat12 <smashkat12@gmail.com>
2020-11-02 21:55:42 +0200

    removed unique property from user model

--- server/models/user.js
+++ server/models/user.js
@@ -16,7 +16,6 @@ const UserSchema = new Schema({
   password: { type: String, visibility: 1 },
   email: {
     type: String,
-    unique: true,
     allowNull: false,
     required: true,
     index: true,
