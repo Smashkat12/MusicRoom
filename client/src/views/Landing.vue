e0a261d515801f12199b54e81506d9efad7fe9e8
Thabang <tchewa@student.wethinkcode.co.za>
2020-11-05 01:38:41 +0200

    get all user playlists

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -43,10 +43,34 @@
       <b-row>
         <b-col sm="12">
             <div>
-              <b-tabs content-class="mt-3" justified>
-                <b-tab title="Charts" active><h3>Charts</h3>
+              <b-tabs content-class="mt-4" fill>
+                <b-tab title="Playlists" active><h3>Playlists</h3>
+                <div class="card-deck" >
+                  <div class="row"  v-if="found_playlists">
+                    <div v-for="(list, index) in found_playlists" v-bind:key="index" @click="send_info(list)">
+                      <router-link v-bind:to="'/info/' + list.title">
+                        <div class="col-4">
+                            <div class="card" border-light style="width: 18rem;">
+                              <img class="card-img-top" :src="list.picture_medium" alt />
+                              <div class="card-body">
+                                <h2>{{ list.title }}</h2>
+                                <h4>Created By {{ list.creator.name }}</h4>
+                                <br />
+                                <br />
+                              </div>
+                          </div>
+                        </div>
+                      </router-link>
+                    </div>
+                  </div>
+                  
+                  <br/><br/>
+                </div>
+              
                 </b-tab>
-                <b-tab title="Playlists"><h3>Playlists</h3></b-tab>
+                <b-tab title="Charts"><h3>Charts</h3>
+                
+                </b-tab>
                 <b-tab title="Listening History"><h3>Listening History</h3></b-tab>
                 <b-tab title="Delegations"><h3>Delegations</h3></b-tab>
               </b-tabs>
@@ -60,7 +84,7 @@
         <div class="d-block text-center">
           <h3>Link your account</h3>
         </div>
-        <b-button
+        <!--<b-button
           class="mt-3"
           href="http://localhost:5000/api/auth/link/google"
           variant="outline-success"
@@ -76,14 +100,7 @@
           @click="hideModal"
           >Link Facebook</b-button
         >
-        <b-button
-          class="mt-3"
-          href="http://localhost:5000/api/auth/link/deezer"
-          variant="outline-success"
-          block
-          @click="hideModal"
-          >Link Deezer</b-button
-        >
+        
         <b-button
           class="mt-3"
           href="http://localhost:8080/login"
@@ -91,6 +108,14 @@
           block
           @click="hideModal"
           >Link Local</b-button
+        >-->
+        <b-button
+          class="mt-3"
+          href="http://localhost:5000/api/auth/link/deezer"
+          variant="outline-success"
+          block
+          @click="hideModal"
+          >Link Deezer</b-button
         >
       </b-modal>
     </div>
@@ -115,6 +140,7 @@ export default {
   data() {
     return {
       title: "",
+      found_playlists: [],
       nameState: null,
       selected: "",
       errors: [],
@@ -138,6 +164,15 @@ export default {
       } else {
         console.log(results.data.success);
         sweet("", "Created " + this.title + " Playlist", "success");
+      }
+    },
+    async getAllUserPlaylists() {
+      try {
+        const res = await axios.get('http://localhost:5000/api/playlist/me/all');
+        this.found_playlists = res.data.playLists;
+        this.no_of_playlist = res.data.playLists.length;
+      } catch (error) {
+        console.error(error);
       }
     },
     async getUserData() {
@@ -200,7 +235,11 @@ export default {
   updated(){
     this.getUserData();
     //this.getAllUserPlaylists();
-  }
+  },
+  created(){
+    this.getUserData();
+    this.getAllUserPlaylists();
+  },
 };
 </script>
 <style scoped>
@@ -209,5 +248,41 @@ export default {
 }
 .right {
   text-align: right;
+}
+
+.center {
+  text-align: center;
+}
+.card-deck {
+    margin: auto 0;
+    text-align: left;
+    margin-left: 16px;
+}
+.card {
+  /* border: solid; */
+  background-color:#b5afbc ;
+  margin-top: 20px;
+}
+
+.sort-buttons {
+  margin: 5px;
+}
+
+.card-img-top {
+  height: 250px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+}
+.card-body {
+  height: 200px;
+  background: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+}
+.card-border{
+  border:yellow;
+}
+.layout {
+   text-align: left;
 }
 </style>
