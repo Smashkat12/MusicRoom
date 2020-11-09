37ec53053d823d2080d9db3cf7e59dd0ec306dcf
mmodisad <50734123+mmodisad@users.noreply.github.com>
2020-11-09 16:22:42 +0200

    user

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -30,55 +30,69 @@
                 </b-form-input>
                 <br />
                 <label for="type">Visibility</label>
-                <b-form-radio v-model="type" name="visibility" value="Public">Public</b-form-radio>
-                <b-form-radio v-model="type" name="visibility" value="Private">Private</b-form-radio>
+                <b-form-radio v-model="type" name="visibility" value="Public"
+                  >Public</b-form-radio
+                >
+                <b-form-radio v-model="type" name="visibility" value="Private"
+                  >Private</b-form-radio
+                >
               </b-form-group>
             </form>
           </b-modal>
         </b-col>
         <b-col sm="4" class="right">
-          <b-button class="buttons" v-b-modal.addUserToPlaylist>Add User To Playlist</b-button> 
+          <b-button class="buttons" v-b-modal.addUserToPlaylist
+            >Add User To Playlist</b-button
+          >
           <b-button class="buttons" v-b-modal.newPlaylist
             >Create Playlist</b-button
           >
         </b-col>
       </b-row>
-      <br/>
+      <br />
       <b-row>
         <b-col sm="12">
-            <div>
-              <b-tabs content-class="mt-4" fill>
-                <b-tab title="Playlists" active><h3>Playlists</h3>
-                <div class="card-deck" >
-                  <div class="row"  v-if="found_playlists">
-                    <div v-for="(list, index) in found_playlists" v-bind:key="index" @click="send_info(list)">
+          <div>
+            <b-tabs content-class="mt-4" fill>
+              <b-tab title="Playlists" active
+                ><h3>Playlists</h3>
+                <div class="card-deck">
+                  <div class="row" v-if="found_playlists">
+                    <div
+                      v-for="(list, index) in found_playlists"
+                      v-bind:key="index"
+                      @click="send_info(list)"
+                    >
                       <router-link v-bind:to="'/playlist/' + list.id">
                         <div class="col-4">
-                            <div class="card" border-light style="width: 18rem;">
-                              <img class="card-img-top" :src="list.picture_medium" alt />
-                              <div class="card-body">
-                                <h2>{{ list.title }}</h2>
-                                <h4>Created By {{ list.creator.name }}</h4>
-                                <br />
-                                <br />
-                              </div>
+                          <div class="card" border-light style="width: 18rem">
+                            <img
+                              class="card-img-top"
+                              :src="list.picture_medium"
+                              alt
+                            />
+                            <div class="card-body">
+                              <h2>{{ list.title }}</h2>
+                              <h4>Created By {{ list.creator.name }}</h4>
+                              <br />
+                              <br />
+                            </div>
                           </div>
                         </div>
                       </router-link>
                     </div>
                   </div>
-                  
-                  <br/><br/>
+
+                  <br /><br />
                 </div>
-              
-                </b-tab>
-                <b-tab title="Charts"><h3>Charts</h3>
-                
-                </b-tab>
-                <b-tab title="Listening History"><h3>Listening History</h3></b-tab>
-                <b-tab title="Delegations"><h3>Delegations</h3></b-tab>
-              </b-tabs>
-            </div>
+              </b-tab>
+              <b-tab title="Charts"><h3>Charts</h3> </b-tab>
+              <b-tab title="Listening History"
+                ><h3>Listening History</h3></b-tab
+              >
+              <b-tab title="Delegations"><h3>Delegations</h3></b-tab>
+            </b-tabs>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -99,24 +113,26 @@
       </b-modal>
 
       <b-modal
-            id="addUserToPlaylist"
-            ref="modal"
-            title="Adding User To Playlist"
-            @show="resetModalUser"
-            @hidden="resetModalUser"
-            @ok="handleOk"
-            okTitle="Add User(s)"
-          >
-            <form ref="form" @submit.stop.prevent="handleSubmitUser">
-              <b-form-group>
-                <label for="type">Playlists</label>
-                <b-form-select v-model="selected" :options="options"></b-form-select>
-                <br /><br />
-                <label for="tags-basic">Users</label>
-                <b-form-tags input-id="tags-basic" v-model="users"></b-form-tags>
-              </b-form-group>
-            </form>
-          </b-modal>
+        id="addUserToPlaylist"
+        ref="modal"
+        title="Adding User To Playlist"
+        @show="resetModalUser"
+        @hidden="resetModalUser"
+        @ok="handleOk"
+        okTitle="Add User(s)"
+      >
+        <form ref="form" @submit.stop.prevent="handleSubmitUser">
+          <b-form-group>
+            <label for="type">Playlists</label>
+              <select name="userPlaylist" v-model="selectedPlaylist" class="form-control" aria-placeholder="Select Playlist">
+                <option v-for="list in playlists" v-bind:key="list.id" v-bind:value="list.id"> {{ list.title }} </option>
+              </select>
+            <br /><br />
+            <label for="tags-basic">Users</label>
+            <b-form-tags input-id="tags-basic" v-model="users"></b-form-tags>
+          </b-form-group>
+        </form>
+      </b-modal>
     </div>
 
     <app-footer></app-footer>
@@ -125,17 +141,17 @@
 <script>
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import {axios_post} from "../functions/functions";
+import { axios_post } from "../functions/functions";
 import sweet from "sweetalert";
 import axios from "axios";
 import EventBus from "../event_bus/event_bus";
-
 
 export default {
   name: "Landing",
   components: {
     "app-header": Header,
     "app-footer": Footer,
+ 
   },
   data() {
     return {
@@ -145,24 +161,25 @@ export default {
       selected: "",
       errors: [],
       success: [],
-      deezerId:"",
-      deezerToken:"",
-      type:"",
-      options:[]
+      deezerId: "",
+      deezerToken: "",
+      type: "",
+      selectedPlaylist:'',
+      playlists: [],
+      playlistId:''
     };
   },
   methods: {
     send_info(title) {
       EventBus.$emit("playlist_details", title);
     },
-    createPlaylist: async function (){
+    createPlaylist: async function () {
       this.error = [];
       const data = {
         title: escape(this.title),
         type: escape(this.type),
         deezerId: escape(this.deezerId),
         deezerToken: escape(this.deezerToken),
-
       };
       var results = await axios_post("/api/playlist/create", data);
       if (results == "Oops!") {
@@ -174,9 +191,10 @@ export default {
     },
     async getAllUserPlaylists() {
       try {
-        const res = await axios.get('http://localhost:5000/api/playlist/me/all');
-        this.found_playlists = res.data.playLists;
-        this.options = res.data.playLists;
+        const res = await axios.get(
+          "http://localhost:5000/api/playlist/me/all"
+        );
+        this.playlists = res.data.playLists;
       } catch (error) {
         console.error(error);
       }
@@ -186,22 +204,21 @@ export default {
       let options = {
         method: "get",
         headers: { Authorization: token },
-        url: "http://localhost:5000/api/auth"
+        url: "http://localhost:5000/api/auth",
       };
       let user = await axios(options).catch(() => {
         console.log("Unable to process request");
       });
-      if(user.data.auth == true)
-      {
-        if(user.data.user._deezerId){
-            this.deezerId = user.data.user._deezerId
-            this.deezerToken = user.data.user.deezerToken;
-            this.hideModal();
-        }else{
+      if (user.data.auth == true) {
+        if (user.data.user._deezerId) {
+          this.deezerId = user.data.user._deezerId;
+          this.deezerToken = user.data.user.deezerToken;
+          this.hideModal();
+        } else {
           this.showModal();
         }
-      }else{
-        console.log("Not Authorised")
+      } else {
+        console.log("Not Authorised");
       }
     },
     checkFormValidity() {
@@ -238,11 +255,11 @@ export default {
       this.$refs["my-modal"].hide();
     },
   },
-  updated(){
+  updated() {
     this.getUserData();
     //this.getAllUserPlaylists();
   },
-  created(){
+  created() {
     this.getUserData();
     this.getAllUserPlaylists();
   },
@@ -255,21 +272,21 @@ export default {
 .right {
   text-align: right;
 }
-.buttons{
-  margin-left:5px;
+.buttons {
+  margin-left: 5px;
 }
 
 .center {
   text-align: center;
 }
 .card-deck {
-    margin: auto 0;
-    text-align: left;
-    margin-left: 16px;
+  margin: auto 0;
+  text-align: left;
+  margin-left: 16px;
 }
 .card {
   /* border: solid; */
-  background-color:#b5afbc ;
+  background-color: #b5afbc;
   margin-top: 20px;
 }
 
@@ -280,7 +297,6 @@ export default {
 .card-img-top {
   height: 250px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-
 }
 .card-body {
   height: 200px;
@@ -288,10 +304,10 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   text-align: center;
 }
-.card-border{
-  border:yellow;
+.card-border {
+  border: yellow;
 }
 .layout {
-   text-align: left;
+  text-align: left;
 }
 </style>
